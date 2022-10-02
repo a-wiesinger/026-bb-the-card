d4760b165fa46f43b31a7562c3b27332c12830d0
Aaron Wiesinger <aaron.wiesinger@gmail.com>
2022-10-02 08:03:30 -0400

    Showing all card permutations

--- Models/Card.cs
+++ Models/Card.cs
@@ -1,8 +1,42 @@
 namespace _026_bb_the_card.Models;
 
-// TODO - Define enums for card colors and card ranks
-
 public class Card
 {
+    // Properties
+    public string NumberOrSymbol
+    {
+        get
+        {
+            // TODO - Check the property of card for number or face
+            return "Result of above check";
+        }
+    }
     
+    
+    // Enumerations
+    public enum CardColors
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow
+    }
+
+    public enum CardRanks
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Dollar,
+        Percent,
+        Caret,
+        Ampersand
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 // :: Level 24 - Boss Battle 3/5 ::
 
 using _026_bb_the_card.Models;
+
+Console.Title = "The Card";
+Console.Clear();
+
+
+// TODO - Create Card instance for each permutation
+foreach (var color in Enum.GetValues(typeof(Card.CardColors)))
+{
+    foreach (var rank in Enum.GetValues(typeof(Card.CardRanks)))
+    {
+        Console.WriteLine(($"The {color} {rank}"));
+    }
+}
